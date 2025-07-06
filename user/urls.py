330857a5ba04fef9abfff9e5d7af01cbca8ece71
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-06 16:03:18 +0300

    Change user creation path users/register/ to users/

--- user/urls.py
+++ user/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create"),
+    path("", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
