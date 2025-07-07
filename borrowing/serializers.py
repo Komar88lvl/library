e6668afefbeca8a2aee055296bfa4c1a5f9f605e
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-07 20:35:47 +0300

    Create BorrowingCreateSerializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,5 +1,7 @@
+from django.db import transaction
 from rest_framework import serializers
 
+from book.models import Book
 from borrowing.models import Borrowing
 from book.serializers import BookSerializer
 
@@ -16,11 +18,10 @@ class BorrowingListSerializer(serializers.ModelSerializer):
             "actual_return_date",
             "user",
             "book",
-
         )
 
 
-class BorrowingRetrieveSerializer(BorrowingListSerializer):
+class BorrowingRetrieveSerializer(serializers.ModelSerializer):
     book = BookSerializer(read_only=True)
     user = serializers.StringRelatedField(read_only=True)
 
@@ -34,3 +35,33 @@ class BorrowingRetrieveSerializer(BorrowingListSerializer):
             "user",
             "book",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all())
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "book",
+            "expected_return_date",
+        )
+        read_only_fields = ("id",)
+
+    def validate(self, attrs):
+        book = attrs["book"]
+
+        if book.inventory <= 0:
+            raise serializers.ValidationError(
+                "This book is not available now"
+            )
+        return attrs
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+        with transaction.atomic():
+            book.inventory -= 1
+            book.save()
+            borrowing = Borrowing.objects.create(**validated_data)
+        return borrowing
