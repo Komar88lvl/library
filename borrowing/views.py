4695462fe9c7b498d56af73cfa50aa7715061088
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-07 18:10:07 +0300

    Overload get_serializers_class to Borrowing viewset for separate showing

--- borrowing/views.py
+++ borrowing/views.py
@@ -2,11 +2,21 @@ from rest_framework import viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer
+from borrowing.serializers import (
+    BorrowingListSerializer,
+    BorrowingRetrieveSerializer
+)
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
-    serializer_class = BorrowingSerializer
+    serializer_class = BorrowingListSerializer
     authentication_classes = (JWTAuthentication,)
     permission_classes = ()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        elif self.action == "retrieve":
+            return BorrowingRetrieveSerializer
+        return BorrowingListSerializer
