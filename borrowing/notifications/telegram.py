70eba479fbe748f95573ad98a9ff9c58e60aaeae
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-08 18:58:59 +0300

    Fix codestyle according pep8

--- borrowing/notifications/telegram.py
+++ borrowing/notifications/telegram.py
@@ -4,6 +4,7 @@ from django.conf import settings
 TELEGRAM_BOT_TOKEN = settings.TELEGRAM_BOT_TOKEN
 TELEGRAM_CHAT_ID = settings.TELEGRAM_CHAT_ID
 
+
 def send_telegram_notifications(message: str):
     if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
         raise ValueError("Set up your .env file")
