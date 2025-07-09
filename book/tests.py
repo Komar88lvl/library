a915c95295e9d752e5752ec63e25b993dd76529c
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-09 14:49:05 +0300

    Create test unauth create_book_forbidden in book app

--- book/tests.py
+++ book/tests.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
+from book.models import Book
 
 BOOK_URL = reverse("book:book-list")
 
@@ -14,3 +15,20 @@ class UnauthenticateBookApiTests(TestCase):
     def test_auth_required(self):
         res = self.client.get(BOOK_URL)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_book_forbidden(self):
+        book = Book.objects.create(
+            title="test title",
+            author="test author",
+            cover="HARD",
+            inventory=5,
+            daily_fee=0.5,
+        )
+
+        payload = {
+            "book": book,
+        }
+
+        res = self.client.post(BOOK_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
