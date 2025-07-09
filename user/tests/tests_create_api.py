69a7e8af2c0b524a528c167727eef784c07c0396
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-09 14:58:11 +0300

    Create test register superuser with email only in user app

--- user/tests/tests_create_api.py
+++ user/tests/tests_create_api.py
@@ -24,3 +24,19 @@ class UnauthenticatedRegister(TestCase):
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         self.assertEqual(user.email, payload["email"])
         self.assertTrue(user.check_password(payload["password"]))
+
+
+class SuperUserRegister(TestCase):
+    def test_create_superuser_with_email_only(self):
+        email = "super@user.test"
+        password = "superpassword"
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+        self.assertEqual(user.username, None)
