b97fdbd42b7b103eeac4507846b2b704c3123bc8
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-09 15:13:19 +0300

    Create test for unauth borrowings access in borrowing app

--- borrowing/tests.py
+++ borrowing/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+
+BORROWING_URL = reverse("borrowing:borrowing-list")
+
+
+
+class UnauthenticatedBorrowingTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(BORROWING_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
