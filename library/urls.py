7423163679be15815fd149324981cce6a43cb7f4
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-09 13:29:33 +0300

    Add spectacular endpoints to global urls.py

--- library/urls.py
+++ library/urls.py
@@ -18,7 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,5 +31,16 @@ urlpatterns = [
     path(
         "api/v1/borrowings/",
         include("borrowing.urls", namespace="borrowing")
+    ),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        'api/v1/doc/swagger/',
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+    path(
+        'api/v1/doc/redoc/',
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
     ),
 ] + debug_toolbar_urls()
