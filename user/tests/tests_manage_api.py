c0beea98d886a65db21cb15e3a3f9ee73e0fe7f8
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-09 15:01:25 +0300

    Create test_authenticated_user_can_update_own_profile in user app

--- user/tests/tests_manage_api.py
+++ user/tests/tests_manage_api.py
@@ -15,3 +15,28 @@ class UnauthenticatedUserManagementTests(TestCase):
     def test_auth_required(self):
         res = self.client.get(USER_MANAGE_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedUserManageTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="test@test.test",
+            password="testpassword",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_authenticated_user_can_update_own_profile(self):
+        payload = {
+            "email": "another@email.test",
+            "password": "anothertestpassword",
+        }
+
+        res = self.client.put(USER_MANAGE_URL, payload)
+        self.user.refresh_from_db()
+        users = get_user_model().objects.all()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(users.count(), 1)
+        self.assertEqual(self.user.email, payload["email"])
+        self.assertTrue(self.user.check_password(payload["password"]))
