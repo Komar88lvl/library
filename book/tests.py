3bea8694cb44e6c9b1cb038f49bad3febc35e214
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-09 14:41:56 +0300

    Create test unauth required to bboks endpoint in book app

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+BOOK_URL = reverse("book:book-list")
+
+
+class UnauthenticateBookApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(BOOK_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
